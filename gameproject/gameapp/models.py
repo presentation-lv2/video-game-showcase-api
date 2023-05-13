87cd81ef4e35a8cb0e378f4b7d3321de058255ef
Miaritiana <hei.miaritiana@gmail.com>
2023-05-13 12:12:17 +0300

    feat:add Game model

--- gameproject/gameapp/models.py
+++ gameproject/gameapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+
+    id = models.IntegerField(primary_key=True,null=False)
+    name = models.CharField(null=False, max_length= 200)
+    description = models.CharField(null=False)
+    realise_date = models.DateField(null=False)
+    image = models.BinaryField()
+
+    def __str__(self):
+        return self.name
+    
+
