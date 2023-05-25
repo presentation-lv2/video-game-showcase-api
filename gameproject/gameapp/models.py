a095ca55cec5a078f8114a23afc69c7400ca864b
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-05-25 14:37:43 +0300

    feat: add users & post model

--- gameproject/gameapp/models.py
+++ gameproject/gameapp/models.py
@@ -11,4 +11,22 @@ class Game(models.Model):
     def __str__(self):
         return self.name
     
+class Users(models.Model):
 
+    id = models.IntegerField(primary_key=True,null=False)
+    username = models.CharField(null=False)
+    role = models.CharField(null=False)
+
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+
+    id = models.IntegerField(primary_key=True,null=False)
+    message = models.CharField(null=False)
+    game_id = models.Foreignkey(Game, related_name='game', on_delete=models.CASCADE)
+    user_id = models.Foreignkey(Users, related_name='users', on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.message
