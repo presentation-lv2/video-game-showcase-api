b919f4c75c6e1c9db34a45969760f24b6e8583e5
Miaritiana <hei.miaritiana@gmail.com>
2023-05-27 11:53:38 +0300

    feat: generate swagger

--- gameproject/gameapp/views.py
+++ gameproject/gameapp/views.py
@@ -1,6 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
 from gameapp.serializers import GameSerializers
 from gameapp.models import Game
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+class SwaggerView:
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Game showcase API",
+            default_version="v1",
+            description="API for game showcase app"
+        ),
+        public= True
+    )
 
 class GameView(ModelViewSet):
 
