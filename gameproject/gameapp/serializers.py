29f50af0b84e1610bbc5e524b57bc5b80de2fa51
Miaritiana <hei.miaritiana@gmail.com>
2023-05-27 11:26:15 +0300

    feat: add game serializers

---
+++ gameproject/gameapp/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer, CharField
+from gameapp.models import Game
+
+class GameSerializers(ModelSerializer):
+
+    image = CharField()
+
+    class Meta:
+        model = Game
+        fields = ["id","name","description","realise_date","image"]
