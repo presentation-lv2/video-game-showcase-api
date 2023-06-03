fcc38f50158d870230d4149e5c142b59ae082c62
Miaritiana <hei.miaritiana@gmail.com>
2023-06-03 09:52:56 +0300

    chore: make id autogenerate

--- gameproject/gameapp/models.py
+++ gameproject/gameapp/models.py
@@ -5,9 +5,9 @@ class Game(models.Model):
 
     id = models.IntegerField(primary_key=True,null=False, auto_created=True)
     name = models.CharField(null=False, max_length= 200)
-    description = models.CharField(null=False)
+    description = models.CharField(null=False, max_length=100)
     realise_date = models.DateField(null=False)
-    layout = models.CharField(null=True)
+    layout = models.CharField(null=True, max_length=50)
     image = models.FileField(upload_to= 'gameapp/static')
 
     def __str__(self):
@@ -35,9 +35,9 @@ class GameCategory(models.Model):
 class Users(models.Model):
 
     id = models.AutoField(primary_key=True,null=False,)
-    username = models.CharField(null=False)
-    password = models.CharField(null=False, default="")
-    role = models.CharField(null=False)
+    username = models.CharField(null=False, max_length=100)
+    password = models.CharField(null=False, default="", max_length=100)
+    role = models.CharField(null=False, max_length=40)
 
     def __str__(self):
         return self.username
@@ -45,8 +45,7 @@ class Users(models.Model):
 class Post(models.Model):
 
     id = models.IntegerField(primary_key=True,null=False)
-    message = models.CharField(null=False)
-
+    message = models.CharField(null=False,max_length=100)
     date = models.DateTimeField(null=False, default=timezone.now)
     game_id = models.ForeignKey(Game, related_name='game', on_delete=models.CASCADE)
     user_id = models.ForeignKey(Users, related_name='users', on_delete=models.CASCADE)
@@ -59,7 +58,7 @@ class Post(models.Model):
 class Target(models.Model):
 
     id = models.IntegerField(primary_key=True,null=False)
-    name = models.CharField(null=False)
+    name = models.CharField(null=False,max_length=50)
 
     def __str__(self):
         return self.name
