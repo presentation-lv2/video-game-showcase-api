ab5d8dc0feaa69e57c6373b8aecdc688f32c8f0f
Miaritiana <hei.miaritiana@gmail.com>
2023-06-03 13:13:11 +0300

    feat: static file root

--- gameproject/gameproject/settings.py
+++ gameproject/gameproject/settings.py
@@ -135,6 +135,8 @@ STATIC_URL = 'gameapp/static/'
 STATICFILE_DIRS = [
     BASE_DIR / "gameapp/static"
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
