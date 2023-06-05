668bebbfb5ad663f0e5e043d15315592c0d3110d
Miaritiana <hei.miaritiana@gmail.com>
2023-06-06 00:35:42 +0300

    fix: slash added

--- gameproject/gameproject/settings.py
+++ gameproject/gameproject/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 MEDIA_ROOT = BASE_DIR / 'gameapp/static'
 
-MEDIA_URL = 'gameapp/static'
+MEDIA_URL = 'gameapp/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
